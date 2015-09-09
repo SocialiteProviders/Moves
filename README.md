9a6d9c9d4e88d97e961bdcffd873988c8119dce9
DraperStudio <drprstd@gmail.com>
2015-09-09 10:30:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Moves OAuth2 Provider for Laravel Socialite
+
+[![Scrutinizer Code Quality](https://img.shields.io/scrutinizer/g/SocialiteProviders/Moves.svg?style=flat-square)](https://scrutinizer-ci.com/g/SocialiteProviders/Moves/?branch=master)
+[![Latest Stable Version](https://img.shields.io/packagist/v/socialiteproviders/moves.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/moves)
+[![Total Downloads](https://img.shields.io/packagist/dt/socialiteproviders/moves.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/moves)
+[![Latest Unstable Version](https://img.shields.io/packagist/vpre/socialiteproviders/moves.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/moves)
+[![License](https://img.shields.io/packagist/l/socialiteproviders/moves.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/moves)
+
+## Documentation
+
+Full documentation for using this provider can be found at [Moves Documentation](http://socialiteproviders.github.io/providers/moves/)
---
+++ src/Provider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace SocialiteProviders\Moves;
+
+use Laravel\Socialite\Two\AbstractProvider;
+use Laravel\Socialite\Two\ProviderInterface;
+use Laravel\Socialite\Two\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['default', 'activity', 'location'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://api.moves-app.com/oauth/v1/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://api.moves-app.com/oauth/v1/access_token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://api.moves-app.com/api/1.1/user/profile', [
+            'headers' => ['Authorization' => 'Bearer '.$token],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['userId'], 'name' => null, 'nickname' => null,
+            'email' => null, 'avatar' => null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+}
